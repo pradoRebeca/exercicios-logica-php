2ad97820fe98ed072791cb8ba7a38f485a24952d
rebeca prado <rebeca.2003.prado@gmail.com>
2021-09-01 10:28:24 -0300

    com função error_igua_maiorl

--- functions/errosPagina.php
+++ functions/errosPagina.php
@@ -2,7 +2,7 @@
     /*aspas dupla fora, e aspas simples dentro */
     const ERRO_CAIXA_VAZIA = "<script> alert('Não é possível calcular sem preencher TODOS os campos') </script>";
 
-    const ERRO_NUMERO_IGUAL = "<script> alert('Não é possivel inserir números iguais. Por favor, selecione outro números') </script>";
+    const ERRO_NUMERO_IGUAL_MAIOR = "<script> alert('Não é possivel inserir valores iguais ou o Valor Inical MAIOR do que o Valor Final. Selecione outro número') </script>";
 
     const ERRO_DADOS_NAO_NUMERICOS = "<script> alert('Não é possível calcular com dados não númericos') </script>";
     
@@ -10,6 +10,6 @@
     
 	const ERRO_DIVISAO_ZERO =  "<script> alert('Não é possivel dividir um número por zero') </script>"; 
 	
-	const ERRO_INICAL_MAIOR = "<script> alert('Não é possível inserir um valor inicial maior do que o valor final') </script>"
+
 	
 ?>
